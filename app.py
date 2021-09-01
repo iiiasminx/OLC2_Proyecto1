0a76b8cb0d3b4945f57c3c1ef989a33a29f21db8
Yasmin <yasminmonterrosoe@gmail.com>
2021-09-01 02:50:24 -0600

    2 ᕙ(`▽´)ᕗ
    Diseño casi terminado - falta codemirror y layout de reportes. Ando viendo que pex para que agarre el js

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/reportes')
+def reportes():
+    return render_template('reportes.html')
+
 @app.route('/submit', methods=['POST'])
 def submit():
     if request.method == 'POST':
-        entrada = request.form['entrada']
-        print(entrada)
-        return
+        #entrada = request.form['entrada']
+        #print(entrada)
+        return jsonify('algo pasa acá')
 
 
 if __name__ == '__main__':
