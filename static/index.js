a2628495370e7e0b2891d49c3860b46640c99715
Yasmin <yasminmonterrosoe@gmail.com>
2021-09-03 01:18:23 -0600

    3 ᕙ(`▽´)ᕗ
    Frontend terminado T.T al fin
    
    ya solo falta todo lo demás xdxd pero si sale!!

--- static/index.js
+++ static/index.js
@@ -1,1 +1,25 @@
-console.log('holi')
+console.log('Fighting!');
+
+$(document).ready(function () {
+    // codigo de Jquery
+
+    var editorEntrada = CodeMirror.fromTextArea(
+      document.getElementById('entrada'), {
+      mode: "julia",
+      theme: "darcula",
+      lineNumbers: true,
+      lineWrapping: false, 
+      readOnly: false
+    });
+    editorEntrada.setSize(null, 300);
+
+    var editorSalida = CodeMirror.fromTextArea(
+      document.getElementById('salida'), {
+      mode: "julia",
+      theme: "darcula",
+      lineNumbers: true,
+      lineWrapping: false, 
+      readOnly: true
+    });
+    editorSalida.setSize(null, 300);
+});
