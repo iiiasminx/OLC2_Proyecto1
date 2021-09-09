d0bd79bf2d759205f2632b7e180c2983df9a0d51
Yasmin <yasminmonterrosoe@gmail.com>
2021-09-08 23:17:26 -0600

    4 ᕙ(`▽´)ᕗ
    Frontend al 1000%

--- app.py
+++ app.py
@@ -21,10 +21,15 @@ def reportes():
 @app.route('/submit', methods=['POST'])
 def submit():
     if request.method == 'POST':
-        #entrada = request.form['entrada']
-        #print(entrada)
-        return jsonify('algo pasa acá')
+        entrada = request.form['entrada']
+        print(entrada)
+        mesg = 'wangxian'
+        return render_template('index.html', mesg=mesg, entrada=entrada)
 
+@app.route('/submit', methods=['GET'])
+def submit2():
+    if request.method == 'GET':
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.debug = True
--- static/index.js
+++ static/index.js
@@ -1,25 +1,43 @@
 console.log('Fighting!');
 
+
 $(document).ready(function () {
-    // codigo de Jquery
+  // codigo de Jquery
 
-    var editorEntrada = CodeMirror.fromTextArea(
-      document.getElementById('entrada'), {
-      mode: "julia",
-      theme: "darcula",
-      lineNumbers: true,
-      lineWrapping: false, 
-      readOnly: false
-    });
-    editorEntrada.setSize(null, 300);
+  var editorEntrada = CodeMirror.fromTextArea(
+    document.getElementById('entrada'), {
+    mode: "julia",
+    theme: "darcula",
+    lineNumbers: true,
+    lineWrapping: false,
+    readOnly: false
+  });
+  editorEntrada.setSize(null, 300);
 
-    var editorSalida = CodeMirror.fromTextArea(
-      document.getElementById('salida'), {
-      mode: "julia",
-      theme: "darcula",
-      lineNumbers: true,
-      lineWrapping: false, 
-      readOnly: true
-    });
-    editorSalida.setSize(null, 300);
+  var editorSalida = CodeMirror.fromTextArea(
+    document.getElementById('salida'), {
+    mode: "julia",
+    theme: "darcula",
+    lineNumbers: true,
+    lineWrapping: false,
+    readOnly: true
+  });
+  editorSalida.setSize(null, 300);
+
+  //labels que no se pueden ver
+  document.getElementById("txtsalida").style.display = 'none';
+  document.getElementById("txtentrada").style.display = 'none';
+
+  if (window.location.href.indexOf("submit") > -1) {
+    //editorSalida.getDoc().setValue('var msg = "Hi";');
+
+    var loquesale = document.getElementById("txtsalida")
+    var textosalida = loquesale.textContent;
+    var loqueentra = document.getElementById("txtentrada")
+    var textoentrada = loqueentra.textContent;
+
+    editorSalida.getDoc().setValue(textosalida);
+    editorEntrada.getDoc().setValue(textoentrada);
+
+  }
 });
